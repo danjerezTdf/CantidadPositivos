39064255a341b64f269242fb4b9f518c7c4f1dd1
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-27 15:29:34 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/* Cantidad y Distribución de Positivos
+• Leer valores del usuario hasta que introduzca un 0
+• El usuario puede introducir valores numéricos, tanto
+positivos como negativos
+• Contar la cantidad de valores introducidos que sean
+mayores a 0 y el porcentaje de positivos respecto del total */
+
 let numeroIngresado: number = Number(prompt(`ingrese valor`));
 let positivos: number = 0;
 let porcentajePos: number = 0;
